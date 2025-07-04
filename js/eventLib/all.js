b1f8c3b637447225220fcbceca6d32f1e62602d3
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-04 10:48:26 +0200

    Add button solver in IHM 2x2 (do nothing for the moment)

--- js/eventLib/all.js
+++ js/eventLib/all.js
@@ -1,4 +1,5 @@
 document.write(`
 <script src="../js/eventLib/load_save_data.js"></script>
 <script src="../js/eventLib/scrambling.js"></script>
+<script src="../js/eventLib/solve_cube.js"></script>
 `);
---
+++ js/eventLib/solve_cube.js
@@ -0,0 +1,6 @@
+
+function solve2x2CubeFunction(){
+    cube = solver.solve(cube);
+    console.log("INFO: solved !");
+    cubeDrawer.draw(canvas, cube);
+}
