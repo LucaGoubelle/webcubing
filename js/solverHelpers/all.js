3f56ec09bd343cf6d6c997b5fa39484e2eb047b9
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-04 09:18:28 +0200

    Add import for solver helpers

--- js/solverHelpers/all.js
+++ js/solverHelpers/all.js
@@ -1,4 +1,16 @@
 document.write(`
+<script src="../js/solverHelpers/CrownCaster.js"></script>
+<script src="../js/solverHelpers/CrownScanner.js"></script>
+<script src="../js/solverHelpers/CubeAlgorithms.js"></script>
+<script src="../js/solverHelpers/CubeColorDetector.js"></script>
+
 <script src="../js/solverHelpers/scanners/Cube2x2Scanner.js"></script>
 <script src="../js/solverHelpers/scanners/Cube3x3Scanner.js"></script>
+
+<script src="../js/solverHelpers/advanced/OLLScanner.js"></script>
+<script src="../js/solverHelpers/advanced/PLLScanner.js"></script>
+
+<script src="../js/solverHelpers/seekers/Corner2Seeker.js"></script>
+<script src="../js/solverHelpers/seekers/Corner3Seeker.js"></script>
+<script src="../js/solverHelpers/seekers/Edge3Seeker.js"></script>
 `);
