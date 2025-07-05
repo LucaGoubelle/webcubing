f8530e5ef66c9578f8b97911e72a9c2e6e48f03f
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-05 13:11:18 +0200

    Add cases in OLL / PLL processors

--- js/solvers/solver2x2/processors/lastFace/oll_processor.js
+++ js/solvers/solver2x2/processors/lastFace/oll_processor.js
@@ -1,7 +1,43 @@
 
 class OLLProcessor {
     constructor(){
-        this.data = {};
+        this.data = {
+            // antisunes cases
+            "00_1010_0001_10_" : CubeAlgorithms.ANTISUNE_ORIENT,
+            "01_0100_0001_10_" : "U "+CubeAlgorithms.ANTISUNE_ORIENT,
+            "01_1000_0010_10_" : "U' "+CubeAlgorithms.ANTISUNE_ORIENT,
+            "01_1000_0101_00_" : "U2 "+CubeAlgorithms.ANTISUNE_ORIENT,
+
+            // sunes cases
+            "10_0001_0100_01_" : CubeAlgorithms.SUNE_ORIENT,
+            "10_0001_1010_00_" : "U "+CubeAlgorithms.SUNE_ORIENT,
+            "00_0101_1000_01_" : "U' "+CubeAlgorithms.SUNE_ORIENT,
+            "10_0010_1000_01_" : "U2 "+CubeAlgorithms.SUNE_ORIENT,
+
+            // U cases
+            "00_1010_1010_00_" : "F R U R' U' F'",
+            "00_0110_0000_11_" : "U F R U R' U' F'",
+            "11_0000_0110_00_" : "U' F R U R' U' F'",
+            "00_0101_0101_00_" : "U2 F R U R' U' F'",
+
+            // T cases
+            "10_0010_0010_10_" : "R U R' U' R' F R F'",
+            "00_0110_1001_00_" : "U R U R' U' R' F R F'",
+            "00_1001_0110_00_" : "U' R U R' U' R' F R F'",
+            "01_0100_0100_01_" : "U2 R U R' U' R' F R F'",
+
+            // L cases
+            "00_0101_0010_10_" : "F R' F' R U R U' R'",
+            "01_0100_1010_00_" : "U2 F R' F' R U R U' R'",
+            "10_0010_0101_00_" : "U F R' F' R U R U' R'",
+            "00_1010_0100_01_" : "U' F R' F' R U R U' R'",
+
+            // Pi cases
+            "01_1000_1000_01_" : CubeAlgorithms.PI_ORIENT,
+            "00_1001_0000_11_" : "U "+CubeAlgorithms.PI_ORIENT,
+            "10_0001_0001_10_" : "U2 "+CubeAlgorithms.PI_ORIENT,
+            "11_0000_1001_00_" : "U' "+CubeAlgorithms.PI_ORIENT
+        };
     }
 
     process(inputData){
--- js/solvers/solver2x2/processors/lastFace/pll_processor.js
+++ js/solvers/solver2x2/processors/lastFace/pll_processor.js
@@ -1,7 +1,19 @@
 
 class PLLProcessor {
     constructor(){
-        this.data = {};
+        this.data = {
+            // T cases
+            "BR_GB_RG_OO" : CubeAlgorithms.T_PERM,
+            "RG_0R_GO_BB" : CubeAlgorithms.T_PERM,
+            "GO_BG_OB_RR" : CubeAlgorithms.T_PERM,
+            "OB_RO_BR_GG" : CubeAlgorithms.T_PERM,
+
+            // Y cases
+
+            "BG_OR_GB_RO" : CubeAlgorithms.Y_PERM,
+            "GB_RO_BG_OR" : CubeAlgorithms.Y_PERM,
+            "RO_BG_OR_GB" : CubeAlgorithms.Y_PERM
+        };
     }
 
     process(inputData){
