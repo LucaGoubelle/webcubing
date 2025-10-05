804b3e0b28f753915160e9afd390fdc9fe7e8b94
LucaGoubelle <luca.goubelle97@gmail.com>
2025-10-05 20:07:26 +0200

    Add some stickers in front face (gigaminx)

--- js/jsminxUI/GigaminxDrawer.js
+++ js/jsminxUI/GigaminxDrawer.js
@@ -15,8 +15,12 @@ class GigaminxDrawer extends MinxDrawer {
     }
 
     _drawFrontFace(canvas, face){
-        //todo: implement this
         drawPolygon(canvas, [[161,128], [196,128], [185,162], [151,161]], this.DICT_COLORS[face[0][0]]);
+        drawPolygon(canvas, [[197,128], [232,128], [221,162], [186,162]], this.DICT_COLORS[face[0][1]]);
+        drawPolygon(canvas, [[232,128], [267,128], [278,162], [221,162]], this.DICT_COLORS[face[0][2]]);
+        drawPolygon(canvas, [[268,128], [303,128], [313,162], [278,162]], this.DICT_COLORS[face[0][3]]);
+        drawPolygon(canvas, [[303,128], [338,128], [348,162], [314,162]], this.DICT_COLORS[face[0][4]]);
+        //todo: implement remaining code
     }
 
     _drawUpFace(canvas, face){
