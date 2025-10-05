4f9f936d8c4553dc457a3f0a2183ee24621a8914
LucaGoubelle <luca.goubelle97@gmail.com>
2025-10-05 18:16:44 +0200

    Add minx data model

---
+++ js/jsminx/data/minx.js
@@ -0,0 +1,23 @@
+
+class Minx {
+
+    constructor(
+        up, front, left, right,
+        downLeft, downRight, absLeft, absRight,
+        backLeft, backRight, back, down
+    ){
+        this.up = up;
+        this.front = front;
+        this.left = left;
+        this.right = right;
+        this.downLeft = downLeft;
+        this.downRight = downRight;
+        this.absLeft = absLeft;
+        this.absRight = absRight;
+        this.backLeft = backLeft;
+        this.backRight = backRight;
+        this.back = back;
+        this.down = down;
+    }
+
+}
