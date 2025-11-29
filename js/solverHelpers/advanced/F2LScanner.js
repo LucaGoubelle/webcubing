c99aaca3bec87e2b1f9af7f03c8fa64612e24f73
LucaGoubelle <luca.goubelle97@gmail.com>
2025-11-29 20:09:27 +0100

    Add some methods in F2LScanner

--- js/solverHelpers/advanced/F2LScanner.js
+++ js/solverHelpers/advanced/F2LScanner.js
@@ -15,4 +15,16 @@ class F2LScanner {
         return result;
     }
 
+    scanIn(cube){
+        let result = cube.front[1][2]+"_"+cube.right[1][0]+"::";
+        result += cube.down[0][2]+"_"+cube.front[2][2]+"_"+cube.right[2][0];
+        return result;
+    }
+
+    scanHalfIn(cube){
+        let result = cube.front[1][2]+"_"+cube.right[1][0]+"::";
+        result += cube.up[2][2]+"_"+cube.front[0][2]+"_"+cube.right[0][0];
+        return result;
+    }
+
 }
