bd89ddf5ef5170bdc9b8d88566b5df106bc39005
LucaGoubelle <luca.goubelle97@gmail.com>
2025-12-28 13:19:14 +0100

    add crown scanner to scanner subpackage

--- js/solverHelpers/all.js
+++ js/solverHelpers/all.js
@@ -1,15 +1,15 @@
 document.write(`
-<script src="../js/solverHelpers/CrownScanner.js"></script>
-<script src="../js/solverHelpers/CubeAlgorithms.js"></script>
-<script src="../js/solverHelpers/CubeColorDetector.js"></script>
-
-<script src="../js/solverHelpers/casters/CrownCaster.js"></script>
-<script src="../js/solverHelpers/casters/centers/CentersCaster.js"></script>
-<script src="../js/solverHelpers/casters/centers/Centers4x4Caster.js"></script>
-<script src="../js/solverHelpers/casters/centers/Centers5x5Caster.js"></script>
-<script src="../js/solverHelpers/casters/centers/Centers6x6Caster.js"></script>
-<script src="../js/solverHelpers/casters/centers/Centers7x7Caster.js"></script>
-
+    <script src="../js/solverHelpers/CubeAlgorithms.js"></script>
+    <script src="../js/solverHelpers/CubeColorDetector.js"></script>
+    
+    <script src="../js/solverHelpers/casters/CrownCaster.js"></script>
+    <script src="../js/solverHelpers/casters/centers/CentersCaster.js"></script>
+    <script src="../js/solverHelpers/casters/centers/Centers4x4Caster.js"></script>
+    <script src="../js/solverHelpers/casters/centers/Centers5x5Caster.js"></script>
+    <script src="../js/solverHelpers/casters/centers/Centers6x6Caster.js"></script>
+    <script src="../js/solverHelpers/casters/centers/Centers7x7Caster.js"></script>
+    
+<script src="../js/solverHelpers/scanners/CrownScanner.js"></script>
 <script src="../js/solverHelpers/scanners/Cube2x2Scanner.js"></script>
 <script src="../js/solverHelpers/scanners/Cube3x3Scanner.js"></script>
 
